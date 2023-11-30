6468ff7c9c864fb11be0ac25698b5305afc08f24
Grace Allen <grace.allen@sinclair.edu>
2023-11-30 15:00:10 -0500

    Filled out Truck.cpp.

--- Truck.cpp
+++ Truck.cpp
@@ -5,4 +5,22 @@
 // Implementation file for Truck class (derived from Vehicle class).
 
 #include "Truck.h"
+#include <iostream>
 using namespace std;
+
+Truck::Truck() {
+	towCap = 0;
+}
+Truck::Truck(long capacity) {
+	towCap = capacity;
+}
+int Truck::getTowCap() {
+	return towCap;
+}
+void Truck::setTowCap(long capacity) {
+	towCap = capacity;
+}
+void Truck::displayInfo() {
+	Vehicle::displayInfo();
+	cout << "Tow Capacity (weight): " << towCap << endl;
+}
