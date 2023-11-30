8589597f61f83d06c5c0e65996e82b6eecf8652d
Grace Allen <grace.allen@sinclair.edu>
2023-11-30 16:13:41 -0500

    Commented through Truck.cpp.

--- Truck.cpp
+++ Truck.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 Truck::Truck() {
-	towCap = 0;
+	towCap = 0; // Automatically sets the Truck object's towing capacity to 0, because this statement is in the object's default constructor.
 }
 Truck::Truck(long capacity) {
 	towCap = capacity;
@@ -21,6 +21,6 @@ void Truck::setTowCap(long capacity) {
 	towCap = capacity;
 }
 void Truck::displayInfo() {
-	Vehicle::displayInfo();
-	cout << "Tow Capacity (weight): " << towCap << endl;
+	Vehicle::displayInfo(); // Inherits Vehicle class's displayInfo() function and uses it to display its inherited variables' values.
+	cout << "Tow Capacity (weight): " << towCap << endl; // Displays its own variable's value.
 }
