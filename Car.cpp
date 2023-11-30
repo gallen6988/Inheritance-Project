ab61ea915ee379ea5a0b58dc887b8992c4bf9500
Grace Allen <grace.allen@sinclair.edu>
2023-11-30 14:37:11 -0500

    Filled out Car.cpp with code for the member functions of the Car class.

--- Car.cpp
+++ Car.cpp
@@ -5,11 +5,19 @@
 // Implementation file for Car class (derived from the Vehicle class).
 
 #include "Car.h"
+#include <iostream>
 using namespace std;
 
 Car::Car() {
 	doorNum = 4;
 }
 int Car::getDoorNum() {
-
+	return doorNum;
+}
+void Car::setDoorNum(int numOfDoors) {
+	doorNum = numOfDoors;
+}
+void Car::displayInfo() {
+	cout << "Number of Doors: " << doorNum << endl;
+	Vehicle::displayInfo();
 }
