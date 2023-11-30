7b377892b79043e1cb484bca706e2e39858b6dcb
Grace Allen <grace.allen@sinclair.edu>
2023-11-30 14:46:59 -0500

    Created and set up Truck.h and Truck.cpp in Truck class to fill out later.

---
+++ Truck.cpp
@@ -0,0 +1,8 @@
+// Grace Allen
+// CIS 1202 800
+// November 30, 2023
+
+// Implementation file for Truck class (derived from Vehicle class).
+
+#include "Truck.h"
+using namespace std;
---
+++ Truck.h
@@ -0,0 +1,15 @@
+// Grace Allen
+// CIS 1202
+// November 30, 2023
+
+// Specification file for Truck class (derived from Vehicle class).
+
+#include "Vehicle.h"
+using namespace std;
+
+#ifndef TRUCK_H
+#define TRUCK_H
+class Truck : public Vehicle {
+
+};
+#endif
