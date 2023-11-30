b277706d630d3b2058be6342dbe575556a2d1827
Grace Allen <grace.allen@sinclair.edu>
2023-11-30 14:20:50 -0500

    Set up Car.h and Car.cpp to start filling them out.

---
+++ Car.h
@@ -0,0 +1,15 @@
+// Grace Allen
+// CIS 1202 800
+// November 30, 2023
+
+// Specification file for Car class (which is derived from the Vehicle class).
+
+#include "Vehicle.h"
+using namespace std;
+
+#ifndef CAR_H
+#define CAR_H
+class Car : public Vehicle {
+
+};
+#endif
